01e4097f383a63a8c4eca3df0d991eef9ff2a26c
leroldary <leroldary@protonmail.com>
2025-07-31 23:07:05 +0200

    Update to eslint flat config and update rules

--- index.js
+++ index.js
@@ -1,27 +1,57 @@
-module.exports = {
-    parser: "@typescript-eslint/parser",
-    env: {
-        browser: true,
-        node: true,
-    },
-    plugins: [
-        "@typescript-eslint",
-    ],
-    extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "prettier",
-    ],
-    rules: {
-        "@typescript-eslint/explicit-function-return-type": "off",
-        "@typescript-eslint/explicit-module-boundary-types": [
-            "error",
-            {
-                "allowArgumentsExplicitlyTypedAsAny": true,
-            }
-        ],
-        "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/no-unused-vars": ["warn", {"argsIgnorePattern": "^_", "varsIgnorePattern": "^_"}],
-    },
-};
+import eslint from "@eslint/js";
+import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
+import importPlugin from "eslint-plugin-import";
+import tseslint from "typescript-eslint";
 
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    importPlugin.flatConfigs.recommended,
+    {
+        rules: {
+            "import/order": [
+                "error", {
+                    "alphabetize": {
+                        "order": "asc",
+                        "caseInsensitive": true,
+                    },
+                    "newlines-between": "always",
+                    "groups": ["builtin", "external", "internal", ["sibling", "parent"], "index", "object", "type"] },
+            ],
+        },
+        settings: {
+            "import/resolver": {
+                "typescript": true,
+                "node": true,
+            },
+        },
+    },
+    {
+        files: ["**/*.ts", "**/*.tsx", "**/*.mts",],
+        extends: [
+            tseslint.configs.recommendedTypeChecked,
+            tseslint.configs.stylisticTypeChecked,
+            importPlugin.flatConfigs.typescript,
+        ],
+        rules: {
+            "@typescript-eslint/explicit-function-return-type": "off",
+            "@typescript-eslint/explicit-module-boundary-types": [
+                "error",
+                {
+                    "allowArgumentsExplicitlyTypedAsAny": true,
+                }
+            ],
+            "@typescript-eslint/no-explicit-any": "off",
+            "@typescript-eslint/no-unused-vars": ["warn", {"argsIgnorePattern": "^_", "varsIgnorePattern": "^_"}],
+        },
+    },
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+    },
+    eslintPluginPrettier,
+);
